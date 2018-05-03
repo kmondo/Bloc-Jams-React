b9ad89c945908ac8c6e5e5297055176aab775a9c
Karen Mondoro <karen.mondoro@comcast.net>
2018-05-03 11:40:01 -0400

    Checkpoint 7 Audio Playback: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,8 +11,38 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
   }
 
   render() {
@@ -35,15 +65,17 @@ class Album extends Component {
             <tbody className="song-list">
               {
                 this.state.album.songs.map( (song, index) =>
-                    <tr>
-                      <td>{index + 1}
-                        <span className="ion-play"></span>
-                        <span className="ion-pause"></span>
+                    <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
+                      <td className="song-actions">
+                        <button>
+                          <span className="song-number">{index + 1}</span>
+                          <span className="ion-play"></span>
+                          <span className="ion-pause"></span>
+                        </button>
                       </td>
-                      <td>{song.title}</td>
-                      <td>{song.duration}</td>
+                      <td className="song-title">{song.title}</td>
+                      <td className="song-duration">{song.duration}</td>
                     </tr>
-
                 )};
             </tbody>
           </table>
