356b006140b344cb46f9be9d4ed8c1ba36c333a3
Karen Mondoro <karen.mondoro@comcast.net>
2018-05-15 11:16:28 -0400

    Checkpoint 10 Styling - Updated Library.css working on Album.css

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
+import '../Library.css';
+
 
 class Library extends Component {
   constructor(props) {
@@ -16,10 +18,12 @@ class Library extends Component {
         {
           this.state.albums.map( (album, index) =>
             <Link to={`/album/${album.slug}`} key={index}>
-              <img src={album.albumCover} alt={album.title} />
+              <img className='albumcover' src={album.albumCover} alt={album.title} />
+              <span className='albuminfo'>
               <div>{album.title}</div>
               <div>{album.artist}</div>
               <div>{album.songs.length} songs</div>
+            </span>
             </Link>
           )
         }
